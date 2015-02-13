84091eb45fd20e6d6dbc02ffca483b33359d0441
Dow  Tang <dowtang1@Dows-MacBook-Air.local>
2015-02-13 15:23:54 +0800

    yelp locations added to google maps

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,11 @@
 class StaticPagesController < ApplicationController
+
+  def index
+  end
+
+  def search
+    parameters = { term: params[:term], limit: 16 }
+    render json: Yelp.client.search('Singapore', parameters)
+  end
+
 end
