98e026215cf284d44240a502a5e41bdb31425b48
Dow  Tang <dowtang1@Dows-MacBook-Air.local>
2015-02-27 17:43:07 +0800

    google prop drop

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -163,6 +163,47 @@ var clearMarkers = function() {
     });
   }
 
+  // var geocoder;
+  // var map;
+  // function initialize() {
+  //   geocoder = new google.maps.Geocoder();
+  //   var latlng = new google.maps.LatLng(-34.397, 150.644);
+  //   var mapOptions = {
+  //     zoom: 8,
+  //     center: latlng
+  //   }
+  //   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  // }
+
+  function codeAddress() {
+    var geocoder = new google.maps.Geocoder();
+    var address = $('#address').val();
+
+    console.log(address);
+
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+
+        console.log(results[0].geometry.location);
+
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+        });
+      } 
+      else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+
+  var controlContainer = $('#geocode_submit')[0];
+  google.maps.event.addDomListener(controlContainer, 'click', function(e) {
+    console.log("button clicked");
+    codeAddress();
+  });
+
   google.maps.event.addDomListener(window, 'load', initialize);
 
 });
