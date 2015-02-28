e5ae6ae8873d11b3a1296776b2c3c65442e14803
Dow  Tang <dowtang1@Dows-MacBook-Air.local>
2015-02-28 19:03:40 +0800

    built user models, improved frontend of devise, loaded 2 gems

--- Gemfile
+++ Gemfile
@@ -35,6 +35,10 @@ gem 'devise'
 
 gem 'nokogiri'
 
+gem 'geokit-rails'
+
+gem 'geocoder'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225033818) do
+ActiveRecord::Schema.define(version: 20150227103048) do
+
+  create_table "likes", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "project_id"
+  end
 
   create_table "listings", force: true do |t|
     t.decimal  "total_price"
